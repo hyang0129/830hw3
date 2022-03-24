1abac27cc8ce5604254015ceb60c1017600a6b1f
hy3134 <hy3134@g.rit.edu>
2022-03-24 15:07:12 -0400

    removed timeout

--- generate_outputs.sh
+++ generate_outputs.sh
@@ -6,6 +6,6 @@ do
 	test_data=sample${i}.in
 	correct_file=sample${i}.out
 	timeout 120s bash compile.sh
-	CUDA_VISIBLE_DEVICES=8 time timeout 60s taskset -c 1-8 bash run.sh ${test_data} correct_file
+	CUDA_VISIBLE_DEVICES=8 time taskset -c 1-8 bash run.sh ${test_data} correct_file
 
 done
