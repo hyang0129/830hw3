da9de089efb241c2d4ac319a455e773b8e7004c5
hy3134 <hy3134@g.rit.edu>
2022-03-24 14:56:52 -0400

    added generator for output

---
+++ generate_outputs.sh
@@ -0,0 +1,11 @@
+
+
+for i in {1..10}
+do
+	printf "\nworking on case ${i}:\n"
+	test_data=sample${i}.in
+	correct_file=sample${i}.out
+	timeout 120s bash compile.sh
+	CUDA_VISIBLE_DEVICES=8 time timeout 60s taskset -c 1-8 bash run.sh ${test_data} correct_file
+
+done
