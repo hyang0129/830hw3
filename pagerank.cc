cf59da554985f1a932c7b60600744eca288bc606
hy3134 <hy3134@g.rit.edu>
2022-03-26 13:25:45 -0400

    fixing as array

--- pagerank.cc
+++ pagerank.cc
@@ -44,19 +44,19 @@ int main(int argc,char** argv){
 
 	//create array equivalents 
 	
-	//int** arr_in_edges; 
-	//arr_in_edges = new int* [V];
-	//for (int i = 0; i < V; ++i) {
-	//	arr_in_edges[V] = new int[longest_in_edges]; 
-	//	for (int j = 0; j < longest_in_edges; j++) {
-	//		if (j < in_edges[i].size()) {
-	//			arr_in_edges[i][j] = in_edges[i][j];
-	//		}
-	//		else {
-	//			arr_in_edges[i][j] = -1; 
-	//		}
+	int** arr_in_edges; 
+	arr_in_edges = new int* [V];
+	for (int i = 0; i < V; ++i) {
+		arr_in_edges[V] = new int[longest_in_edges]; 
+		for (int j = 0; j < longest_in_edges; j++) {
+			if (j < in_edges[i].size()) {
+				arr_in_edges[i][j] = in_edges[i][j];
+			}
+			else {
+				arr_in_edges[i][j] = -1; 
+			}
 
-	//	}
+		}
 	//}
 
 	//int* arr_out_degree;
