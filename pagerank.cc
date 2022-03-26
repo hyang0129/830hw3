f68dc7cd1430da5c48a1901581e2de2fb0b7ced7
hy3134 <hy3134@g.rit.edu>
2022-03-26 14:03:32 -0400

    pr as array

--- pagerank.cc
+++ pagerank.cc
@@ -4,80 +4,32 @@
 #include<queue>
 #include<vector>
 
-
 const double d = 0.85;
-int V,E,L,M;
+int V, E, L, M;
 
 std::vector<std::vector<int>> in_edges;
 std::vector<int> out_degree;
 
-
-int main(int argc,char** argv){
-	FILE* fin = fopen(argv[1],"r");
-	FILE* fout = fopen(argv[2],"w");
-	fscanf(fin,"%d%d%d%d",&V,&E,&L,&M);
+int main(int argc, char** argv) {
+	FILE* fin = fopen(argv[1], "r");
+	FILE* fout = fopen(argv[2], "w");
+	fscanf(fin, "%d%d%d%d", &V, &E, &L, &M);
 	in_edges.resize(V);
-	out_degree = std::vector<int>(V,0);
+	out_degree = std::vector<int>(V, 0);
 
-	int longest_in_edges = 0; 
-	int num_edges = 0; 
-	for(int i = 0;i < E;++i){
-		int u,v;
-		fscanf(fin,"%d%d",&u,&v);
+	for (int i = 0; i < E; ++i) {
+		int u, v;
+		fscanf(fin, "%d%d", &u, &v);
 		in_edges[v].push_back(u);
 		++out_degree[u];
-
-		// compute longest edge
-		if (in_edges[v].size() > longest_in_edges) {
-			longest_in_edges = in_edges[v].size();
-		}
-
-		++num_edges;
 	}
 
 	std::vector<double> pr[2];
 	pr[0].resize(V);
 	pr[1].resize(V);
 	int current = 0;
-	for(int i = 0;i < V;++i){
+	for (int i = 0; i < V; ++i) {
 		pr[current][i] = 1.0 / V;
-	}
-
-	//create array equivalents 
-	
-	int flat_edges[num_edges]; 
-	int flat_edge_locations[V + 1]; 
-
-	int current_edge = 0; 
-
-	for (int i = 0; i < V; ++i) {
-		flat_edge_locations[i] = current_edge;
-
-		for (int j = 0; j < in_edges[i].size(); j++) {
-
-			flat_edges[current_edge] = in_edges[i][j];
-			++current_edge;
-		}
-	}
-	flat_edge_locations[V + 1] = num_edges; 
-
-	int arr_in_edges[V][longest_in_edges];
-	
-	for (int i = 0; i < V; ++i) {
-		for (int j = 0; j < longest_in_edges; j++) {
-			if (j < in_edges[i].size()) {
-				arr_in_edges[i][j] = in_edges[i][j];
-			}
-			else {
-				arr_in_edges[i][j] = -1; 
-			}
-
-		}
-	}
-
-	int arr_out_degree[V];
-	for (int i = 0; i < V; ++i) {
-		arr_out_degree[i] = out_degree[i];
 	}
 
 	double arr_pr[2][V];
@@ -86,37 +38,24 @@ int main(int argc,char** argv){
 		arr_pr[current][i] = 1.0 / V;
 	}
 
-
-	////cuda allocate PR 
-
-
 	for (int iter = 0; iter < M; ++iter) {
 		int next = 1 - current;
 		for (int i = 0; i < V; ++i) {
 			double sum = 0;
-			//for (int j = 0; j < in_edges[i].size(); ++j) {
-			//	int v = in_edges[i][j];
-			//	sum += pr[current][v] / out_degree[v];
-			//}
-
-			int flat_edge_start = flat_edge_locations[i];
-			int flat_edge_end = flat_edge_locations[i + 1];
-		
-			for (int j = flat_edge_start; j < flat_edge_end; ++j) {
-				int v = flat_edges[j];
+			for (int j = 0; j < in_edges[i].size(); ++j) {
+				int v = in_edges[i][j];
 				sum += pr[current][v] / out_degree[v];
 			}
-
-
-			pr[next][i] = (1.0 - d) / V + d * sum;
+			arr_pr[next][i] = (1.0 - d) / V + d * sum;
 		}
+
+		
+
 		current = next;
 	}
 
-
-
-	for(int i = 0;i < V;++i){
-		fprintf(fout,"%.8f\n", pr[current][i]);
+	for (int i = 0; i < V; ++i) {
+		fprintf(fout, "%.8f\n", arr_pr[current][i]);
 	}
 	fclose(fin);
 	fclose(fout);
--- pagerank.cu
+++ pagerank.cu
@@ -4,7 +4,6 @@
 #include<queue>
 #include<vector>
 
-using namespace std;
 
 const double d = 0.85;
 int V, E, L, M;
@@ -21,7 +20,7 @@ int main(int argc, char** argv) {
 	out_degree = std::vector<int>(V, 0);
 
 	int longest_in_edges = 0;
-
+	int num_edges = 0;
 	for (int i = 0; i < E; ++i) {
 		int u, v;
 		fscanf(fin, "%d%d", &u, &v);
@@ -33,6 +32,7 @@ int main(int argc, char** argv) {
 			longest_in_edges = in_edges[v].size();
 		}
 
+		++num_edges;
 	}
 
 	std::vector<double> pr[2];
@@ -45,10 +45,25 @@ int main(int argc, char** argv) {
 
 	//create array equivalents 
 
-	int** arr_in_edges;
-	arr_in_edges = new int* [V];
+	int flat_edges[num_edges];
+	int flat_edge_locations[V + 1];
+
+	int current_edge = 0;
+
 	for (int i = 0; i < V; ++i) {
-		arr_in_edges[V] = new int[longest_in_edges];
+		flat_edge_locations[i] = current_edge;
+
+		for (int j = 0; j < in_edges[i].size(); j++) {
+
+			flat_edges[current_edge] = in_edges[i][j];
+			++current_edge;
+		}
+	}
+	flat_edge_locations[V + 1] = num_edges;
+
+	int arr_in_edges[V][longest_in_edges];
+
+	for (int i = 0; i < V; ++i) {
 		for (int j = 0; j < longest_in_edges; j++) {
 			if (j < in_edges[i].size()) {
 				arr_in_edges[i][j] = in_edges[i][j];
@@ -60,49 +75,48 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	int* arr_out_degree;
-	arr_out_degree = new int[V];
+	int arr_out_degree[V];
 	for (int i = 0; i < V; ++i) {
 		arr_out_degree[i] = out_degree[i];
 	}
 
-	double** arr_pr;
-	arr_pr = new double* [2];
-	arr_pr[0] = new double[V];
-	arr_pr[1] = new double[V];
+	double arr_pr[2][V];
 
 	for (int i = 0; i < V; ++i) {
 		arr_pr[current][i] = 1.0 / V;
 	}
 
 
-	//cuda allocate PR 
-	//cudaMallocManaged(&arr_pr, 2 * V * sizeof(double));
-	//cudaMallocManaged(&arr_out_degree, V * sizeof(int));
-	//cudaMallocManaged(&arr_in_edges, V * longest_in_edges * sizeof(int));
+	////cuda allocate PR 
 
 
 	for (int iter = 0; iter < M; ++iter) {
 		int next = 1 - current;
 		for (int i = 0; i < V; ++i) {
-
 			double sum = 0;
-			for (int j = 0; j < longest_in_edges; ++j) {
-				int v = arr_in_edges[i][j];
+			//for (int j = 0; j < in_edges[i].size(); ++j) {
+			//	int v = in_edges[i][j];
+			//	sum += pr[current][v] / out_degree[v];
+			//}
 
-				if (v > -1) {
-					sum += arr_pr[current][v] / arr_out_degree[v];
-				}
+			int flat_edge_start = flat_edge_locations[i];
+			int flat_edge_end = flat_edge_locations[i + 1];
+
+			for (int j = flat_edge_start; j < flat_edge_end; ++j) {
+				int v = flat_edges[j];
+				sum += pr[current][v] / out_degree[v];
 			}
 
 
-			arr_pr[next][i] = (1.0 - d) / V + d * sum;
+			pr[next][i] = (1.0 - d) / V + d * sum;
 		}
 		current = next;
 	}
 
+
+
 	for (int i = 0; i < V; ++i) {
-		fprintf(fout, "%.8f\n", arr_pr[current][i]);
+		fprintf(fout, "%.8f\n", pr[current][i]);
 	}
 	fclose(fin);
 	fclose(fout);
