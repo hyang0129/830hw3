d235b04c99df4bef8ef464e1745cc34ec97cce90
hy3134 <hy3134@g.rit.edu>
2022-03-26 18:06:15 -0400

    adding cuda

--- pagerank.cu
+++ pagerank.cu
@@ -51,9 +51,8 @@ __global__ void oneVertex(int i,
 
 
 	if (idx == 0) {
-		arr_pr[i + next * V] = r[0];
-
-		//arr_pr[i + next * V] = (1.0 - d) / V + d * r[0];
+	
+		arr_pr[i + next * V] = (1.0 - d) / V + d * r[0];
 	}
 	
 
