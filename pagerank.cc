c2380c3ac6d79f6aa4f5d21028f98bfad1556284
hy3134 <hy3134@g.rit.edu>
2022-03-26 14:49:18 -0400

    as arr

--- pagerank.cc
+++ pagerank.cc
@@ -45,22 +45,6 @@ int main(int argc, char** argv) {
 
 	//create array equivalents 
 
-	//int flat_edges[num_edges];
-	//int flat_edge_locations[V + 1];
-
-	//int current_edge = 0;
-
-	//for (int i = 0; i < V; ++i) {
-	//	flat_edge_locations[i] = current_edge;
-
-	//	for (int j = 0; j < in_edges[i].size(); j++) {
-
-	//		flat_edges[current_edge] = in_edges[i][j];
-	//		++current_edge;
-	//	}
-	//}
-	//flat_edge_locations[V + 1] = num_edges;
-
 	int** arr_in_edges = (int**) malloc(V * sizeof(int*));
 
 	for (int i = 0; i < V; ++i) {
