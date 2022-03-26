11fbe148b1e0c2b5ea16157e44d0ec42c1c4a4b3
hy3134 <hy3134@g.rit.edu>
2022-03-26 17:34:25 -0400

    adding cuda

--- pagerank.cu
+++ pagerank.cu
@@ -133,31 +133,31 @@ int main(int argc, char** argv) {
 		int next = 1 - current;
 
 		for (int i = 0; i < V; ++i) {
-			//double sum = 0;
+			double sum = 0;
 
-			//for (int j = edge_starts[i]; j < edge_starts[i + 1]; ++j) {
-			//	int v = flat_edges[j];
-			//	
-			//	sum += arr_pr[v + current * V] / arr_out_degree[v];
-			//	
-			//}
+			for (int j = edge_starts[i]; j < edge_starts[i + 1]; ++j) {
+				int v = flat_edges[j];
+				
+				sum += arr_pr[v + current * V] / arr_out_degree[v];
+				
+			}
 
-			//arr_pr[i + next * V] = (1.0 - d) / V + d * sum;
+			arr_pr[i + next * V] = (1.0 - d) / V + d * sum;
 			
 
-			oneVertex << <1, blockSize >> > (
-				i,
-				V,
-				d,
-				next,
-				current,
-				flat_edges,
-				edge_starts,
-				arr_out_degree,
-				arr_pr
-				);
+			//oneVertex << <1, blockSize >> > (
+			//	i,
+			//	V,
+			//	d,
+			//	next,
+			//	current,
+			//	flat_edges,
+			//	edge_starts,
+			//	arr_out_degree,
+			//	arr_pr
+			//	);
 
-			cudaDeviceSynchronize();
+			//cudaDeviceSynchronize();
 			
 		}
 		current = next;
