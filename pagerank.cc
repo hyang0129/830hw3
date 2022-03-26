1f2b084d0654c730dc500061b4849dc481ce9478
hy3134 <hy3134@g.rit.edu>
2022-03-26 13:23:58 -0400

    fixing as array

--- pagerank.cc
+++ pagerank.cc
@@ -44,57 +44,71 @@ int main(int argc,char** argv){
 
 	//create array equivalents 
 	
-	int** arr_in_edges; 
-	arr_in_edges = new int* [V];
-	for (int i = 0; i < V; ++i) {
-		arr_in_edges[V] = new int[longest_in_edges]; 
-		for (int j = 0; j < longest_in_edges; j++) {
-			if (j < in_edges[i].size()) {
-				arr_in_edges[i][j] = in_edges[i][j];
-			}
-			else {
-				arr_in_edges[i][j] = -1; 
-			}
+	//int** arr_in_edges; 
+	//arr_in_edges = new int* [V];
+	//for (int i = 0; i < V; ++i) {
+	//	arr_in_edges[V] = new int[longest_in_edges]; 
+	//	for (int j = 0; j < longest_in_edges; j++) {
+	//		if (j < in_edges[i].size()) {
+	//			arr_in_edges[i][j] = in_edges[i][j];
+	//		}
+	//		else {
+	//			arr_in_edges[i][j] = -1; 
+	//		}
 
-		}
-	}
+	//	}
+	//}
 
-	int* arr_out_degree;
-	arr_out_degree = new int[V];
-	for (int i = 0; i < V; ++i) {
-		arr_out_degree[i] = out_degree[i];
-	}
+	//int* arr_out_degree;
+	//arr_out_degree = new int[V];
+	//for (int i = 0; i < V; ++i) {
+	//	arr_out_degree[i] = out_degree[i];
+	//}
 
-	double** arr_pr;
-	arr_pr = new double* [2];
-	arr_pr[0] = new double[V];
-	arr_pr[1] = new double[V];
+	//double** arr_pr;
+	//arr_pr = new double* [2];
+	//arr_pr[0] = new double[V];
+	//arr_pr[1] = new double[V];
 
-	for (int i = 0; i < V; ++i) {
-		arr_pr[current][i] = 1.0 / V;
-	}
-
-
-	//cuda allocate PR 
+	//for (int i = 0; i < V; ++i) {
+	//	arr_pr[current][i] = 1.0 / V;
+	//}
 
 
+	////cuda allocate PR 
 
 
-	for(int iter = 0;iter < M;++iter){
+
+
+	//for(int iter = 0;iter < M;++iter){
+	//	int next = 1 - current;
+	//	for(int i = 0;i < V;++i){
+
+	//		double sum = 0;
+	//		for (int j = 0; j < longest_in_edges; ++j) {
+	//			int v = arr_in_edges[i][j];
+
+	//			if (v > -1) {
+	//				sum += arr_pr[current][v] / arr_out_degree[v];
+	//			}
+	//		}
+
+
+	//		arr_pr[next][i] = (1.0 - d) / V + d * sum;
+	//	}
+	//	current = next;
+	//}
+
+
+	for (int iter = 0; iter < M; ++iter) {
 		int next = 1 - current;
-		for(int i = 0;i < V;++i){
-
+		for (int i = 0; i < V; ++i) {
 			double sum = 0;
-			for (int j = 0; j < longest_in_edges; ++j) {
-				int v = arr_in_edges[i][j];
-
-				if (v > -1) {
-					sum += arr_pr[current][v] / arr_out_degree[v];
-				}
+			for (int j = 0; j < in_edges[i].size(); ++j) {
+				int v = in_edges[i][j];
+				sum += pr[current][v] / out_degree[v];
 			}
-
-
-			arr_pr[next][i] = (1.0 - d) / V + d * sum;
+			pr[next][i] = (1.0 - d) / V + d * sum;
 		}
 		current = next;
 	}
