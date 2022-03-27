d3d34588773f4c93f926eeea2d2b3ecb31187a2d
hy3134 <hy3134@g.rit.edu>
2022-03-26 22:29:27 -0400

    adding super cuda

--- pagerank.cc
+++ pagerank.cc
@@ -4,13 +4,11 @@
 #include<queue>
 #include<vector>
 
-
 const double d = 0.85;
 int V, E, L, M;
 
 std::vector<std::vector<int>> in_edges;
 std::vector<int> out_degree;
-
 
 int main(int argc, char** argv) {
 	FILE* fin = fopen(argv[1], "r");
@@ -19,20 +17,11 @@ int main(int argc, char** argv) {
 	in_edges.resize(V);
 	out_degree = std::vector<int>(V, 0);
 
-	int longest_in_edges = 0;
-	int num_edges = 0;
 	for (int i = 0; i < E; ++i) {
 		int u, v;
 		fscanf(fin, "%d%d", &u, &v);
 		in_edges[v].push_back(u);
 		++out_degree[u];
-
-		// compute longest edge
-		if (in_edges[v].size() > longest_in_edges) {
-			longest_in_edges = in_edges[v].size();
-		}
-
-		++num_edges;
 	}
 
 	std::vector<double> pr[2];
@@ -43,46 +32,35 @@ int main(int argc, char** argv) {
 		pr[current][i] = 1.0 / V;
 	}
 
-	//create array equivalents 
-	int** arr_in_edges = (int**)malloc(V * sizeof(int*));
-	int arr_in_edges_count[V]; 
-
-
-	for (int i = 0; i < V; ++i) {
-		arr_in_edges[i] = (int*)malloc(in_edges[i].size() * sizeof(int));
-		arr_in_edges_count[i] = in_edges[i].size();
-		for (int j = 0; j < in_edges[i].size(); j++) {
-			arr_in_edges[i][j] = in_edges[i][j];
-
-		}
-	}
-
-
-	int arr_out_degree[V];
-	for (int i = 0; i < V; ++i) {
-		arr_out_degree[i] = out_degree[i];
-	}
-
 	double arr_pr[2][V];
 
 	for (int i = 0; i < V; ++i) {
 		arr_pr[current][i] = 1.0 / V;
 	}
 
-	////cuda allocate PR 
-
-
 	for (int iter = 0; iter < M; ++iter) {
 		int next = 1 - current;
 		for (int i = 0; i < V; ++i) {
 			double sum = 0;
-			for (int j = 0; j < arr_in_edges_count[i]; ++j) {
-				int v = arr_in_edges[i][j];
-				sum += arr_pr[current][v] / arr_out_degree[v];
+			for (int j = 0; j < in_edges[i].size(); ++j) {
+				int v = in_edges[i][j];
+				sum += arr_pr[current][v] / out_degree[v];
 			}
-
 			arr_pr[next][i] = (1.0 - d) / V + d * sum;
 		}
+
+		int same = 1;
+		for (int i = 0; i < V; ++i) {
+			if (arr_pr[current][i] != arr_pr[next][i]) {
+				same = 0;
+			}
+		}
+
+		if (same == 1) {
+			break;
+		}
+
+
 		current = next;
 	}
 
