866fa1fad15b09181e4c5619b8b2574e6ff7ee6b
hy3134 <hy3134@g.rit.edu>
2022-03-26 21:10:40 -0400

    adding cuda

--- pagerank.cu
+++ pagerank.cu
@@ -368,6 +368,17 @@ int main(int argc, char** argv) {
 
 	//// super cuda 
 
+	int device = -1;
+	cudaGetDevice(&device);
+
+	cudaMemPrefetchAsync(flat_edges, E * sizeof(int), device, NULL);
+	cudaMemPrefetchAsync(edge_starts, (V + 1) * sizeof(int), device, NULL);
+	cudaMemPrefetchAsync(arr_out_degree, V * sizeof(int), device, NULL);
+	cudaMemPrefetchAsync(arr_pr, 2 * V * sizeof(double), device, NULL);
+	cudaMemPrefetchAsync(cu_edge_sections, (total_edge_sections + 1) * sizeof(int), device, NULL);
+	cudaMemPrefetchAsync(cu_edge_section_to_vertex, total_edge_sections * sizeof(int), device, NULL);
+	cudaMemPrefetchAsync(sections_result, total_edge_sections * sizeof(double), device, NULL);
+	cudaMemPrefetchAsync(cu_vertex_section_starts, (V + 1) * sizeof(int), device, NULL);
 
 
 	for (int iter = 0; iter < M; ++iter) {
